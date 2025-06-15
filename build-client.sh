46d84140d5eece476bc9070f4dbdd16d9dfee149
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2025-06-15 08:51:40 +0530

    Update build client script

--- build-client.sh
+++ build-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "building client"
-cd ../tank-game-client/ && npm run build && cp -r dist/ ../tank-game-server/client
+rm -rf client && cd ../tank-game-client/ && npm run build && cp -r dist ../tank-game-server/client
 echo "done"
