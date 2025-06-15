be431d7a4b769ac2e74eb6e829f66e76a168d35a
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2025-06-15 08:53:25 +0530

    Create hub to manage players and events

---
+++ hub.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var wsUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type hub struct {
+	players    map[*player]bool
+	evHandlers map[string]func(ev event, c *player) error
+	mu         sync.RWMutex
+}
+
+func (h *hub) setupEventHandlers() {
+	h.evHandlers[EventNewPlayer] = handleNewPlayerEvent
+}
+
+func (h *hub) handleEvents(ev event, p *player) error {
+	evHandler, ok := h.evHandlers[ev.Type]
+	if !ok {
+		return fmt.Errorf("unsupported event type")
+	}
+
+	err := evHandler(ev, p)
+	if err != nil {
+		log.Printf("error handling event: %v\n", err)
+		return err
+	}
+
+	return nil
+}
+
+func (h *hub) addToPlayers(p *player) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	h.players[p] = true
+}
+
+func (h *hub) removeFromPlayers(p *player) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	_, ok := h.players[p]
+	if ok {
+		p.connection.Close()
+		close(p.egress)
+		delete(h.players, p)
+	}
+}
+
+func (h *hub) getAllPlayers() []*player {
+	players := []*player{}
+	for p := range h.players {
+		players = append(players, p)
+	}
+	return players
+}
+
+func (h *hub) serveWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("error upgrading to websocket protocol: %v\n", err)
+		return
+	}
+
+	c := newPlayer(conn, h)
+	h.addToPlayers(c)
+
+	go c.readMessages()
+	go c.writeMessages()
+}
+
+func newHub() *hub {
+	h := &hub{
+		players:    make(map[*player]bool),
+		evHandlers: make(map[string]func(ev event, c *player) error),
+		mu:         sync.RWMutex{},
+	}
+	h.setupEventHandlers()
+	return h
+}
